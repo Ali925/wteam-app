946e61cc5f2310ced87f1e7bd639d5def3e2de9f
Ali925 <alitural.mehdi@gmail.com>
2020-10-18 22:50:04 +0400

    last

---
+++ notatize.js
@@ -0,0 +1,11 @@
+const notarize = require('electron-notarize');
+
+async function packageTask () {
+  // Package your app here, and code sign with hardened runtime
+  await notarize({
+    appBundleId: "com.wteam.chat",
+    appPath: "./dist/wteam-0.0.14.dmg",
+    appleId: "comb@mail.ru",
+    appleIdPassword: "hjjh-vsvt-izzm-zout"
+  });
+}
